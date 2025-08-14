6288dbfc07f722c136913e86e74c38420b2617df
ghostminhtoan <ghostminhtoan@gmail.com>
2025-08-14 13:33:30 +0700

    windows setup.cmd

--- windows setup.cmd
+++ windows setup.cmd
@@ -1,106 +1,123 @@
 @echo off
 chcp 65001 >nul
-setlocal ENABLEDELAYEDEXPANSION
-
-:lang_select
 cls
-echo ================================================
-echo Select language / Chọn ngôn ngữ
-echo ================================================
+
+:MAIN_MENU
+echo.
+echo ==============================
+echo    WINDOWS INSTALLATION MENU
+echo    MENU CAI DAT WINDOWS
+echo ==============================
 echo 1. English
-echo 2. Tiếng Việt
-echo.
-set /p lang=Choice / Lựa chọn: 
-if "%lang%"=="1" goto main_menu_en
-if "%lang%"=="2" goto main_menu_vi
-goto lang_select
+echo 2. Tieng Viet
+echo ==============================
+set /p choice="Select your language/Chon ngon ngu (1/2): "
 
-:: ========== ENGLISH MENU ==========
-:main_menu_en
-cls
-echo ================================================
-echo Windows Installation Tool - ENGLISH
-echo ================================================
-echo 1. Select target disk to install Windows
-echo 2. Select location of install.wim
-echo z. Back
-echo.
-set /p choice=Your choice: 
-if /i "%choice%"=="1" goto select_disk_en
-if /i "%choice%"=="2" goto select_wim_en
-if /i "%choice%"=="z" goto lang_select
-goto main_menu_en
+if "%choice%"=="1" goto ENGLISH
+if "%choice%"=="2" goto VIETNAMESE
+goto MAIN_MENU
 
-:select_disk_en
+:ENGLISH
+set lang=EN
+set drive_text=Select disk to install Windows (e.g. 0,1,2...)
+set format_text=Format this disk? (Y/N)
+set format_note=Note: Y=Yes, N=No
+set wim_text=Enter path to install.wim file
+set wim_guide=Guide: Right-click ISO -> Mount -> Look in mounted drive (e.g. E:\sources\install.wim)
+set back_text=Press 'z' to go back
+set invalid_text=Invalid choice, please try again
+set install_text=Installing Windows...
+set success_text=Installation completed successfully!
+goto DISK_SELECT
+
+:VIETNAMESE
+set lang=VI
+set drive_text=Chon o cung de cai Windows (vd: 0,1,2...)
+set format_text=Co format o cung nay khong? (Y/N)
+set format_note=Chu y: Y=Co, N=Khong
+set wim_text=Nhap duong dan toi file install.wim
+set wim_guide=Huong dan: Chuot phai vao file ISO -> Mount -> Tim trong o dia vua mount (vd: E:\sources\install.wim)
+set back_text=Nhan 'z' de quay lai
+set invalid_text=Lua chon khong hop le, vui long thu lai
+set install_text=Dang cai dat Windows...
+set success_text=Cai dat thanh cong!
+goto DISK_SELECT
+
+:DISK_SELECT
 cls
-echo List of disks:
-diskpart /s "%~f0".list >nul 2>&1
-echo (Dummy) Please manually list disks using diskpart if needed.
-set /p disknum=Enter target disk number: 
-set /p fmt=Do you want to format the disk? (Y=Yes / N=No): 
-if /i "%fmt%"=="y" (
-    echo Formatting disk %disknum% ...
-    rem put diskpart commands here
-) else (
-    echo Skipping format.
+echo.
+echo ==============================
+if "%lang%"=="EN" echo    WINDOWS INSTALLATION - DISK SELECTION
+if "%lang%"=="VI" echo    CAI DAT WINDOWS - CHON O CUNG
+echo ==============================
+echo %drive_text%
+echo %back_text%
+echo ==============================
+diskpart /s "%~dp0list_disks.txt" | find "Disk ###"
+set /p disk="Disk number/So thu tu o dia: "
+
+if "%disk%"=="z" goto MAIN_MENU
+
+:FORMAT_PROMPT
+cls
+echo.
+echo ==============================
+if "%lang%"=="EN" echo    FORMAT DISK %disk%?
+if "%lang%"=="VI" echo    FORMAT O DIA %disk%?
+echo ==============================
+echo %format_text%
+echo %format_note%
+echo ==============================
+set /p format="(Y/N): "
+
+if /i "%format%"=="z" goto DISK_SELECT
+if /i "%format%"=="y" (
+    echo select disk %disk% > format.txt
+    echo clean >> format.txt
+    echo convert gpt >> format.txt
+    echo create partition primary >> format.txt
+    echo format fs=ntfs quick >> format.txt
+    echo active >> format.txt
+    echo assign >> format.txt
+    diskpart /s format.txt
+    del format.txt
+) else if /i "%format%" neq "n" (
+    echo %invalid_text%
+    timeout /t 2 >nul
+    goto FORMAT_PROMPT
 )
-pause
-goto main_menu_en
 
-:select_wim_en
+:WIM_SELECT
 cls
-echo Instructions:
-echo Right-click your Windows ISO, choose "Mount".
-echo Find the new drive letter (e.g., E:\).
-echo The install.wim will be in E:\sources\install.wim
-set /p wimpath=Enter full path to install.wim: 
-echo You entered: %wimpath%
-pause
-goto main_menu_en
-
-:: ========== VIETNAMESE MENU ==========
-:main_menu_vi
-cls
-echo ================================================
-echo Công cụ cài đặt Windows - TIẾNG VIỆT
-echo ================================================
-echo 1. Chọn ổ đĩa để cài Windows
-echo 2. Chọn nơi lưu file install.wim
-echo z. Quay lại
 echo.
-set /p choice=Nhập lựa chọn: 
-if /i "%choice%"=="1" goto select_disk_vi
-if /i "%choice%"=="2" goto select_wim_vi
-if /i "%choice%"=="z" goto lang_select
-goto main_menu_vi
+echo ==============================
+if "%lang%"=="EN" echo    SELECT INSTALL.WIM LOCATION
+if "%lang%"=="VI" echo    CHON VI TRI FILE INSTALL.WIM
+echo ==============================
+echo %wim_text%
+echo %wim_guide%
+echo %back_text%
+echo ==============================
+set /p wim_path="Path/Duong dan: "
 
-:select_disk_vi
-cls
-echo Danh sách ổ đĩa:
-diskpart /s "%~f0".list >nul 2>&1
-echo (Ví dụ) Hãy dùng lệnh diskpart để xem danh sách ổ đĩa nếu cần.
-set /p disknum=Nhập số ổ đĩa: 
-set /p fmt=Bạn có muốn format ổ này không? (Y=Đồng ý / N=Không): 
-if /i "%fmt%"=="y" (
-    echo Đang format ổ %disknum% ...
-    rem put diskpart commands here
-) else (
-    echo Bỏ qua format.
+if "%wim_path%"=="z" goto DISK_SELECT
+if not exist "%wim_path%" (
+    echo %invalid_text%
+    timeout /t 2 >nul
+    goto WIM_SELECT
 )
-pause
-goto main_menu_vi
 
-:select_wim_vi
+:INSTALLATION
 cls
-echo Hướng dẫn:
-echo Chuột phải file Windows ISO, chọn "Mount".
-echo Xem ổ đĩa mới xuất hiện (ví dụ: E:\).
-echo File install.wim nằm trong E:\sources\install.wim
-set /p wimpath=Nhập đường dẫn đầy đủ tới install.wim: 
-echo Bạn đã nhập: %wimpath%
-pause
-goto main_menu_vi
+echo.
+echo ==============================
+echo %install_text%
+echo ==============================
+dism /apply-image /imagefile:"%wim_path%" /index:1 /applydir:\\.\PhysicalDrive%disk%
 
-endlocal
-exit /b
-
+echo.
+echo ==============================
+echo %success_text%
+echo ==============================
+timeout /t 5
+exit
