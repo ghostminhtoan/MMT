b69dd2fbbe83215b72c09b6ae3e9a5d88cf3bcfd
ghostminhtoan <ghostminhtoan@gmail.com>
2025-08-19 16:45:50 +0700

    FastStoneCaptureKeygen.ps1

--- FastStoneCaptureKeygen.ps1
+++ FastStoneCaptureKeygen.ps1
@@ -1,22 +1,22 @@
 <#
 .SYNOPSIS
-    Script cai dat IDM crack
+    Script cai dat FastStone Capture Keygen
 .DESCRIPTION
-    Thuc hien tai va cai dat IDM crack
+    Thuc hien tai va cai dat FastStone Capture Keygen
 #>
 
 # 1. Tai va cai dat FastStone Capture Keygen
 Write-Host "Dang tai va cai dat FastStone Capture Keygen..."
-$idmInstaller = "$env:TEMP\keygen.exe"
+$keygenInstaller = "$env:TEMP\FastStoneKeygen.exe"
 try {
-    Invoke-RestMethod "https://raw.githubusercontent.com/ghostminhtoan/MMT/refs/heads/main/FastStone%20Capture%20Keygen.exe" -OutFile $idmInstaller
+    Invoke-RestMethod "https://raw.githubusercontent.com/ghostminhtoan/MMT/refs/heads/main/FastStone%20Capture%20Keygen.exe" -OutFile $keygenInstaller
     $process = Start-Process -FilePath $keygenInstaller -PassThru
     $process.WaitForExit()
-    Remove-Item $idmInstaller -Force
-    Write-Host "Da cai dat Keygen thanh cong" -ForegroundColor Green
+    Remove-Item $keygenInstaller -Force
+    Write-Host "Da cai dat FastStone Capture Keygen thanh cong" -ForegroundColor Green
 }
 catch {
-    Write-Host "Loi khi cai dat Keygen: $_" -ForegroundColor Red
+    Write-Host "Loi khi cai dat FastStone Capture Keygen: $_" -ForegroundColor Red
 }
 
 Write-Host "Hoan tat thao tac" -ForegroundColor Cyan
