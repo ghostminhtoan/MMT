c392699251c7ad250c086b855a713e5fecdb774e
ghostminhtoan <ghostminhtoan@gmail.com>
2025-12-27 18:18:38 +0700

    Update GMTPC.Tool.ps1

--- GMTPC.Tool.ps1
+++ GMTPC.Tool.ps1
@@ -1,21 +1,19 @@
-# Đường dẫn thư mục
-$TempFolder = "$env:LOCALAPPDATA\Temp\GMTPC.Tool"
+# Duong dan thu muc
+$TempFolder = "$env:LOCALAPPDATA\GMTPC\GMTPC Tools"
 $ExePath = Join-Path $TempFolder "GMTPC.Tool.exe"
-$Url = "https://github.com/ghostminhtoan/private/releases/download/MMT/GMTPC.Tool.exe"
+$Url = "https://github.com/ghostminhtoan/private/releases/download/MMT/GMTPC.Tool.exe"  # <-- Dat URL trong dau nhay kep
 
-# 1. Tạo thư mục MMTPC
+# 1. Tao thu muc GMTPC Tool
 if (!(Test-Path $TempFolder)) {
-    New-Item -ItemType Directory -Path $TempFolder | Out-Null
+    Write-Host "Tao thu muc: $TempFolder"
+    New-Item -ItemType Directory -Path $TempFolder -Force | Out-Null
 }
 
+# 3. Tai file ve
+Invoke-WebRequest -Uri $Url -OutFile $ExePath -UseBasicParsing
 
-
-# 3. Tải file về
-Invoke-WebRequest -Uri $Url -OutFile $ExePath
-
-# 4. Chạy file và chờ nó đóng
+# 4. Chay file va doi no dong
 Start-Process -FilePath $ExePath -Wait
 
-
-# 6. Xoá thư mục MMTPC
+# 6. Xoa thu muc GMTPC Tool
 Remove-Item -Path $TempFolder -Recurse -Force
